d233dcd1f312aa0b8a05e1bff3db271039ff6417
pc <businesselswah@gmail.com>
2019-03-11 21:16:42 +0400

    add pojo permissions dependecies files

--- app/src/main/java/com/gemini/block/geminitask/service/NewsService.java
+++ app/src/main/java/com/gemini/block/geminitask/service/NewsService.java
@@ -1,4 +1,15 @@
 package com.gemini.block.geminitask.service;
 
-public class NewsService {
+import com.gemini.block.geminitask.model.New;
+
+import java.util.HashMap;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.QueryMap;
+
+ public interface NewsService {
+    @GET("everything")
+    Call<New> getNews(@QueryMap HashMap<String, String> filters);
 }
--- app/src/main/java/com/gemini/block/geminitask/views/MainActivity.java
+++ app/src/main/java/com/gemini/block/geminitask/views/MainActivity.java
@@ -4,11 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.gemini.block.geminitask.R;
+import com.gemini.block.geminitask.model.New;
+import com.gemini.block.geminitask.service.NewsService;
+import com.gemini.block.geminitask.service.ServiceBuilder;
+import com.gemini.block.geminitask.utils.Constants;
 
+import java.util.HashMap;
 import java.util.Locale;
 
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +27,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         unbinder = ButterKnife.bind(this);
         Timber.d(Locale.getDefault().getLanguage());
+        HashMap<String, String> filterMap = new HashMap<>();
+        filterMap.put("q", "bitcoin");
+        filterMap.put("apiKey", Constants.token);
+        NewsService newsService = ServiceBuilder.buildService(NewsService.class);
+        Call<New> request = newsService.getNews(filterMap);
+       request.enqueue(new Callback<New>() {
+           @Override
+           public void onResponse(Call<New> call, Response<New> response) {
+               Timber.d(response.toString());
+
+           }
+
+           @Override
+           public void onFailure(Call<New> call, Throwable t) {
+
+           }
+       });
     }
 
     @Override
